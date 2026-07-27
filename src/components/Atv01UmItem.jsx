d6a0b34cd6e1f1a664538c87a7d89e0e8871beca
Arthur <arthur.machado@nave.org.br>
2026-07-27 14:43:00 -0300

    Implement fetch to load user data in Atv01UmItemasd

--- src/components/Atv01UmItem.jsx
+++ src/components/Atv01UmItem.jsx
@@ -22,3 +22,63 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+import { useState } from 'react'
+
+const estilo = {
+  usuario: {
+    backgroundColor: "#dac",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+}
+
+export default function Atv01UmItem (){
+  const [resultado, setResultado] = useState(<p>Os dados do usuário aparecerão no lugar deste texto</p>)
+
+  async function Carregar() {
+      await fetch(
+        'https://jsonplaceholder.typicode.com/todos/1', 
+        { method: 'GET', }
+      )
+      .then((resposta) => {
+          console.log(resposta)
+          return resposta.json()
+        })
+        .then((resultado) => {
+          console.log(resultado)
+          let status = ""
+          if(resultado.status == true){
+            status = "feito"
+          }
+          else{
+            status = "a fazer"
+          }
+          const usuario = <div 
+          style={{margin: "10px", padding: "5px", backgroundColor: "#a89", borderRadius: "5px",}}>
+            <p>{resultado.id} - {resultado.title}: {status}</p>
+          </div>
+
+          setResultado(usuario)
+        })
+      }
+    
+    
+    return(
+      <div style={estilo.usuario}>
+        <p>Um item:</p>
+        <button style={estilo.botao} onClick={() => Carregar()}><p style={estilo.textoBotao}>Clique abaixo para carregar uma atividade</p></button>
+        {resultado}
+      </div>
+    )
+}
