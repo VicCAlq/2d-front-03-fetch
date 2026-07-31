f4cf0a499b83237b5a86a3c74c47c7f5f36d8b34
Vinicyus-Nave2D <vinicyus.eduardo@nave.org.br>
2026-07-31 15:34:00 -0300

    finish

--- src/components/Atv03TratarErrosDeUm.jsx
+++ src/components/Atv03TratarErrosDeUm.jsx
@@ -27,3 +27,77 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+
+
+
+
+
+import { useState } from 'react'
+
+const estilo = {
+  usuario: {
+    backgroundColor: "#dac",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+}
+
+
+export default function Atv03TratarErrosDeUm(){
+const [resultado, setResultado] = useState(<p>Os dados do usuário aparecerão no lugar deste texto</p>)
+
+  async function carregarUsuario(){
+    await fetch(
+     'https://jsonplaceholder.typicode.com/comments/20',
+     {method: 'GET',}
+    )
+    .then((resposta)=>{
+      if(!resposta.ok){
+        throw new Error (`Erro na requisição! Status: ${resposta.status}`);
+      }
+      return resposta.json()
+    })
+    .then((resul)=>{
+      console.log(resul)
+      const usu =  <div  style={{
+        margin: "10px", padding: "5px", backgroundColor: "#a89", borderRadius: "5px",
+      }}>
+        <p>{resul.postId} - {resul.id}: {resul.email}</p>
+        <p>{resul.name}</p>
+        <p>{resul.body}</p>
+      </div>
+      setResultado(usu)
+    })
+    .catch(error => {
+      console.log("Erro: ", error)
+    })
+  }
+  return( 
+      <div style={estilo.usuario}>
+
+      <p>
+        Carregue o usuário abaixo:
+      </p>
+
+      <button style={estilo.botao} onClick={() => carregarUsuario()}>
+        <p style={estilo.textoBotao}>Carregar usuário</p>
+      </button>
+
+      <div>
+      {resultado}
+      </div>
+      
+    </div>
+  )
+}
--- src/components/Atv04TratarErrosDeMuitos.jsx
+++ src/components/Atv04TratarErrosDeMuitos.jsx
@@ -28,3 +28,77 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+
+import { useState } from 'react'
+
+const estilo = {
+  usuario: {
+    backgroundColor: "#dac",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+}
+
+
+export default function Atv04TratarErrosDeMuitos(){
+const [resultado, setResultado] = useState(<p>Os dados do usuário aparecerão no lugar deste texto</p>)
+
+  async function carregarUsuario(){
+    await fetch(
+     'https://jsonplaceholder.typicode.com/comments',
+     {method: 'GET',}
+    )
+    .then((resposta)=>{
+      if(!resposta.ok){
+        throw new Error (`Erro na requisição! Status: ${resposta.status}`);
+      }
+      return resposta.json()
+    })
+    .then((resul)=>{
+      console.log(resul)
+      const listausu = <div> {resul.map((resul) =>
+      {
+         return <div  style={{
+        margin: "10px", padding: "5px", backgroundColor: "#a89", borderRadius: "5px",
+      }}>
+        <p>{resul.postId} - {resul.id}: {resul.email}</p>
+        <p>{resul.name}</p>
+        <p>{resul.body}</p>
+      </div>
+      })}
+      </div>
+      setResultado(listausu)
+    })
+    .catch(error => {
+      console.log("Erro: ", error)
+    })
+  }
+  return( 
+      <div style={estilo.usuario}>
+
+      <p>
+        Carregue o usuário abaixo:
+      </p>
+
+      <button style={estilo.botao} onClick={() => carregarUsuario()}>
+        <p style={estilo.textoBotao}>Carregar usuário</p>
+      </button>
+
+      <div>
+      {resultado}
+      </div>
+      
+    </div>
+  )
+}
