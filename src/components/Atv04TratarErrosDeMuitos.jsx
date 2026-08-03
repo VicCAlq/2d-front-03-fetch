74bec77c635e763ef910de23d44a94d731027785
brendha-priscilla-nave <brendha.priscilla@nave.org.br>
2026-08-03 13:50:04 -0300

    atividade 4 ok

--- src/components/Atv04TratarErrosDeMuitos.jsx
+++ src/components/Atv04TratarErrosDeMuitos.jsx
@@ -28,3 +28,49 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+
+import {useState} from "react"
+
+export default  Atv04TratarErrosDeMuitos()[
+
+  const [comentarios, setComentarios] = useState (
+    <p> Aqui serão recebidos os comentários</p>
+  )
+
+  async function carregarComentarios() {
+    await fetch(
+      " https://jsonplaceholder.typicode.com/comments",
+      {method: "GET"}
+    )                                                                                                                                                                                                                                                                                                                                                                     
+
+    .then((resposta) => {
+      if(!resposta.ok){
+        throw new Error(`Error na requisição: ${resposta.status}`);
+      }
+      return resposta.json()  
+    })
+    .then((listaDeComentarios) => {
+      setComentarios(<div>
+        {listaDeComentarios.map((comentario) => {
+          return(<div>
+            <p>{comentario.postId}: {comntario.id} - {comentario.email}</p>
+            <p>{comentario.name}</p>
+            <p>{comentario.body}</p>
+          </div>)
+        })}
+      </div>)
+
+      })
+      .catch ((erro) => {
+        window.alert(erro)
+    })
+  }
+  return(
+    <div>
+      <button onClick={()=> carregarComentarios() }>
+        Clique abaixo para carregar a atividade
+      </button>
+      {comentarios}
+    </div>
+  )
+]
