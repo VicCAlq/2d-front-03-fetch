30c878b317fe1b649ace93737d168299ca6c023d
Filipedaspaçoca <adriano.santos@nave.org.br>
2026-08-04 11:00:14 -0300

    1,2 e 4

--- src/components/Atv02MuitosItens.jsx
+++ src/components/Atv02MuitosItens.jsx
@@ -23,3 +23,83 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+
+
+/*
+  * ATIVIDADE 01: UM ITEM
+  *
+  * Crie e exporte por padrão um componente chamado Atv01UmItem, que deve ter
+  * uma <div>, e dentro desta <div> um <button> com o conteúdo 
+  * "Clique abaixo para carregar uma atividade", que quando pressionado
+  * fará uma requisição usando "fetch" para a URL abaixo:
+  *
+  * https://jsonplaceholder.typicode.com/todos/1
+  *
+  * Esta URL envia um objeto JSON com as propriedades:
+  * userId: número inteiro
+  * id: número inteiro
+  * title: texto
+  * completed: booleano
+  *
+  * Ao receber este conteúdo, ele deve ser exibido da seguinte forma
+  * dentro de um elemento <p> abaixo do <button>:
+  * [id] - [title]: [status]
+  * Os colchetes indicam que deve se tratar de uma variável, e não
+  * do texto dentro deles.
+  * O valor de "status" deve ser "feito" se completed for true, 
+  * ou "a fazer" se completed for false
+  */
+
+import {useState} from "react";
+
+export default function Atv01UmItem() {
+  const [resposta, setResposta]  = useState ([])
+
+async function carregarAtividade() {
+  const response = await fetch(  "https://jsonplaceholder.typicode.com/todos/1", { method: "GET" }
+  )
+
+  const resultado = await response.json();
+ 
+
+  let status;
+if (resultado.completed) {
+    status = "feito";
+} else {status = "a fazer"};
+
+
+  const textos = resultado.map ((item) => {
+
+let status;
+
+if (item.completed) {
+  status = "feito";
+} else {
+  status = "a fazer"
+}
+
+return `${item.id}` - ${item.title}: ${status};
+
+})
+
+setResposta(texto);
+  }
+
+return (
+  <div>
+
+    <button onClick={carregarAtividade}>
+      Clique abaixo para carregar uma atividade
+    </button>
+
+<div> 
+
+  {resposta.map((texto, index) => (
+  <p key ={index} >{texto}</p>
+  ))}
+
+</div>
+    </div>
+);
+
+}
