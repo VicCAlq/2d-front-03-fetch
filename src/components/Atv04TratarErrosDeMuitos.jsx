2d0d141067ab1bfa2662b9c2aa3c29be0d0570ad
bernardo-nascimento-nave <bernardo.cavalcanti@nave.org.br>
2026-08-05 21:21:19 -0300

    .

--- src/components/Atv04TratarErrosDeMuitos.jsx
+++ src/components/Atv04TratarErrosDeMuitos.jsx
@@ -1,3 +1,6 @@
+Novo Documento de Texto.txt
+1
+100%
 /*
   * ATIVIDADE 04: TRATAR ERROS DE MUITOS
   *
@@ -28,3 +31,54 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+import {useState} from "react"
+export default function Atv04TratarErrosDeMuitos() {
+
+  const [comentarios, setComentarios ] = useState(<p>
+    Aqui serão recebidos os comentários
+    </p>)
+    await fetch(
+    "https://jsonplaceholder.typicode.com/comments",
+    { method: "GET" }
+    )
+    .then((resposta)=> {
+      if (!resposta.ok) {
+        throw new Error('Erro na requisição: ${resposta.status}');
+        
+      }
+      return resposta.json()
+    })
+    .then((listaDeComentarios)=> {
+      setComentarios(<div
+        style={{
+          display: "flex", flexDirection:"column", gap:"10px",
+          width: "500px", margin: "10px auto",
+        }}
+        >
+        {listaDeComentarios.map((comentario) => {
+          return(<div 
+            style={{
+              backgroundColor: "#77aaee",
+            }}
+            >
+               <p>[comentario.postId]: [comentario.id] - [comentario.email]</p>
+    <p>[comentario.name]</p>
+    <p>[comentario.body]</p>
+    </div>)
+        })}
+      </div>)
+    })
+  .catch((erro) => {
+    window.alert(erro)
+  })
+
+
+  return(
+  <div>
+    <button onClick={() => carregarComentarios()}>
+   Clique abaixo para carregar uma atividade 
+    </button>
+    {comentarios}
+  </div>
+ )
+}
