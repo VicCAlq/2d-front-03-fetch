0e43a28f400c32398c62460ac3861ee28f87b501
clarice-vitoria-nave <clarice.vitoria@nave.org.br>
2026-08-10 14:36:08 -0300

    Implement error handling in data fetching component

--- src/components/Atv03TratarErrosDeUm.jsx
+++ src/components/Atv03TratarErrosDeUm.jsx
@@ -26,4 +26,63 @@
   * <p>[body]</p>
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
-  */
+  */import { useState } from 'react'
+
+const estilo = {
+  tarefa: {
+    backgroundColor: "#dac",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+}
+
+export default function Atv03TratarErrosDeUm(){
+  const [resultado, setResultado] = useState(<p>Os dados do usuário aparecerão no lugar deste texto</p>)
+  async function Carregar(){
+    await fetch(
+        'https://jsonplaceholder.typicode.com/comments/20', 
+        { method: 'GET', }
+      )
+      .then((resposta) => {
+        if (!resposta.ok) {
+        throw new Error(`Erro na requisição! Status: ${resposta.status}`);
+      }
+        console.log(resposta)
+        return resposta.json()
+        })
+    .then((resultado) => {
+      console.log(resultado)
+      const usuario = <div style={{
+        margin: "10px", padding: "5px", backgroundColor: "#6a7", borderRadius: "5px",
+      }}>
+        <p>{resultado.postId}: {resultado.id} - {resultado.email}</p>
+        <p>{resultado.name}</p>
+        <p>{resultado.body}</p>
+      </div>
+      setResultado(usuario)
+    })
+    .catch(error => {
+      console.log("Erro: ", error)
+    })
+  }
+  
+  return(
+    <div style={estilo.tarefa}>
+      <button onClick={() => Carregar()}
+      style={estilo.botao}><p style={estilo.textoBotao}>Clique abaixo para carregar uma atividade</p></button>
+      {resultado}
+    </div>
+  )
+}
+
