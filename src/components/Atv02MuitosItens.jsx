daa2a6e3ed9f1527457d20504f26ccd80afea4ce
bianca-beatriz-nave <bianca.beatriz@nave.org.br>
2026-08-21 09:55:59 -0300

    Update Atv02MuitosItens.jsx

--- src/components/Atv02MuitosItens.jsx
+++ src/components/Atv02MuitosItens.jsx
@@ -23,3 +23,65 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+
+import { useState } from "react"
+
+const estilo = {
+  tarefa: {
+    backgroundColor: "#dac",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+}
+
+export default function Atv02MuitosItens(){
+const [resultado, setResultado] = useState(<p>Os dados do usuário aparecerão no lugar deste texto</p>)
+    async function Carregar() {
+      await fetch(
+        'https://jsonplaceholder.typicode.com/todos', 
+        { method: 'GET', }
+      )
+      .then((resposta) => {
+          console.log(resposta)
+          return resposta.json()
+        })
+      .then((resultado) => {
+        
+        const lista = <>
+          {resultado.map(tarefa => {
+
+            let status = ""
+            if (tarefa.status == true){
+              status = "feito"
+            }
+            else{
+              status = "a fazer"
+            }
+            return <div 
+            style={{margin: "10px", padding: "5px", backgroundColor: "#a89", borderRadius: "5px",}}>
+              <p>{tarefa.id} - {tarefa.title}: {status}</p>
+            </div>
+          })}
+        </>
+          setResultado(lista)
+      })
+    }
+  return(
+    <div style={estilo.tarefa}>
+      <button style={estilo.botao} 
+      onClick={() => Carregar()}><p style={estilo.textoBotao}>Clique abaixo para carregar várias atividades</p></button>
+      {resultado}
+    </div>
+  )
+}
