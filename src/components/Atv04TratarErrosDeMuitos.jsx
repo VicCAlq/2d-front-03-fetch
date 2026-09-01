4d26d910443266e8a3ac34cb6496f61e0fcbb628
Kaylane-Marinho-nave <kaylane.marinho@nave.org.br>
2026-09-01 15:01:25 -0300

    feito

--- src/components/Atv04TratarErrosDeMuitos.jsx
+++ src/components/Atv04TratarErrosDeMuitos.jsx
@@ -28,3 +28,65 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+import { useState } from 'react'
+
+const estilo = {
+  tarefa: {
+    backgroundColor: "#dac",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+}
+
+export default function Atv03TratarErrosDeUm(){
+  const [resultado, setResultado] = useState(<p>Os dados do usuário aparecerão no lugar deste texto</p>)
+  async function Carregar(){
+    await fetch(
+        'https://jsonplaceholder.typicode.com/comments', 
+        { method: 'GET', }
+      )
+      .then((resposta) => {
+        if (!resposta.ok) {
+        throw new Error(`Erro na requisição! Status: ${resposta.status}`);
+      }
+        console.log(resposta)
+        return resposta.json()
+        })
+    .then((resultado) => {
+      const lista = <>
+        {resultado.map(tarefa => {
+          return <div style={{
+            margin: "10px", padding: "5px", backgroundColor: "#6a7", borderRadius: "5px",
+          }}>
+            <p>{resultado.postId}: {resultado.id} - {resultado.email}</p>
+            <p>{resultado.name}</p>
+            <p>{resultado.body}</p>
+          </div>
+        })}
+      </>
+      setResultado(lista)
+    })
+    .catch(error => {
+      console.log("Erro: ", error)
+    })
+  }
+  
+  return(
+    <div style={estilo.tarefa}>
+      <button onClick={() => Carregar()}
+      style={estilo.botao}><p style={estilo.textoBotao}>Clique abaixo para carregar uma atividade</p></button>
+      {resultado}
+    </div>
+  )
+}
